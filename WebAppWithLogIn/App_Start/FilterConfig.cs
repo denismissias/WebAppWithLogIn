36a6d28260acdec65602f780abbd2c6e65063b6d
Denis Missias <denis.missias@gmail.com>
2018-07-05 11:05:00 -0300

    Initial commit

---
+++ WebAppWithLogIn/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppWithLogIn
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
